43f04be0541dcf636f431b9c68a32012ada8f5af
nipusapu <nipuna.saputhanthri@gmail.com>
2018-08-11 09:16:42 +0530

    change api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:8080',
+  apiUrl: 'https://node-translation.herokuapp.com/',
 
   dialogflow:{ 
     chatbot:'529cffb125e54cd1904d4f78c4ce4219'
